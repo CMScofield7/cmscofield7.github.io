cf2031db90586e33ea8675f50752fdfdb8bb4718
CM Scofield <scoundreldays2@gmail.com>
2025-02-17 07:49:08 -0300

    Alterações importantes no código - nomenclatura de imagem e vite.config

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,24 @@
-import SocialMedia from './SocialMedia'
-import Contact from './Contact'
+import SocialMedia from "./SocialMedia";
+import Contact from "./Contact";
 
-import Eu from '../img/Eu, só que em JPEG.jpg'
+import Eu from "../img/eu.jpg";
 
-import '../styles/components/sidebar.sass'
+import "../styles/components/sidebar.sass";
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Eu} alt="CM Scofield" />
-      <p className="title" style={{ color: '#FFD100' }}>Desenvolvedor Backend</p>
+      <p className="title" style={{ color: "#FFD100" }}>
+        Desenvolvedor Backend
+      </p>
       <SocialMedia />
       <Contact />
-      <a href="" className="btn">Aqui está meu currículo!</a>
+      <a href="" className="btn">
+        Aqui está meu currículo!
+      </a>
     </aside>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
